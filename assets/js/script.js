d1e16fffa7879c6dbc187620d1222d9a9c071e3e
AnjinIsmail <anjin.ismail@gmail.com>
2020-10-15 13:48:36 -0700

    feature/drag-drop added

--- assets/js/script.js
+++ assets/js/script.js
@@ -58,7 +58,7 @@ $(".list-group").on("blur", "textarea", function () {
 
   // get the task's position in the list of other li elements
   var index = $(this).closest(".list-group-item").index();
-  
+
   //<<<< tasks[status][index].text = text;>>>>> originial doesnt work intermitently with .text   when i remove the .text it works also intermitently... WHY!>
   tasks[status][index] = text;
   saveTasks();
@@ -81,7 +81,7 @@ $(".list-group").on("click", "span", function () {
     .addClass("form-control")
     .val(date);
 
-    //swap out elements 
+  //swap out elements 
   $(this).replaceWith(dateInput);
 
   //automatcially focus on new element 
@@ -112,48 +112,113 @@ $(".list-group").on("blur", "input[type='text']", function () {
   $(this).replaceWith(taskSpan);
 });
 
-// modal was triggered
-$("#task-form-modal").on("show.bs.modal", function () {
-  // clear values
-  $("#modalTaskDescription, #modalDueDate").val("");
-});
 
-// modal is fully visible
-$("#task-form-modal").on("shown.bs.modal", function () {
-  // highlight textarea
-  $("#modalTaskDescription").trigger("focus");
-});
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolernace: "pointer",
+  helper: "clone",
+  activate: function(event) {
+    console.log("activate", this);
+  },
+  deactivate: function(event) {
+    console.log("deactivate", this);
+  },
+  over: function(event) {
+    console.log("over", event.target);
+  },
+  out: function(event) {
+    console.log("out", event.target);
+  },
+  update: function(event) {
+    //array to store the task data in 
+    var tempArr= []; 
+    //loop over current set of children in sortable list 
+    $(this).children().each(function () {
+      var text = $(this)
+      .find("p")
+      .text()
+      .trim();
 
-// save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function () {
-  // get form values
-  var taskText = $("#modalTaskDescription").val();
-  var taskDate = $("#modalDueDate").val();
-
-  if (taskText && taskDate) {
-    createTask(taskText, taskDate, "toDo");
-
-    // close modal
-    $("#task-form-modal").modal("hide");
-
-    // save in tasks array
-    tasks.toDo.push({
-      text: taskText,
-      date: taskDate,
+      var date =$(this)
+      .find("span")
+      .text()
+      .trim();
+      
+      // add task data to the temp array as an object
+      tempArr.push ({
+        text: text,
+        date: date
+      
+      });
     });
+    //trim down list's ID to match object property
+    var arrName = $(this)
+    .attr("id")
+    .replace("list-", "");
 
+    //update array on tasks object and save
+    tasks[arrName] = tempArr;
     saveTasks();
   }
 });
+  // console.log(tempArr);
 
-// remove all tasks
-$("#remove-tasks").on("click", function () {
-  for (var key in tasks) {
-    tasks[key].length = 0;
-    $("#list-" + key).empty();
+  // modal was triggered
+  $("#task-form-modal").on("show.bs.modal", function () {
+    // clear values
+    $("#modalTaskDescription, #modalDueDate").val("");
+  });
+
+  // modal is fully visible
+  $("#task-form-modal").on("shown.bs.modal", function () {
+    // highlight textarea
+    $("#modalTaskDescription").trigger("focus");
+  });
+
+  // save button in modal was clicked
+  $("#task-form-modal .btn-primary").click(function () {
+    // get form values
+    var taskText = $("#modalTaskDescription").val();
+    var taskDate = $("#modalDueDate").val();
+
+    if (taskText && taskDate) {
+      createTask(taskText, taskDate, "toDo");
+
+      // close modal
+      $("#task-form-modal").modal("hide");
+
+      // save in tasks array
+      tasks.toDo.push({
+        text: taskText,
+        date: taskDate,
+      });
+
+      saveTasks();
+    }
+  });
+
+  // remove all tasks
+  $("#remove-tasks").on("click", function () {
+    for (var key in tasks) {
+      tasks[key].length = 0;
+      $("#list-" + key).empty();
+    }
+    saveTasks();
+  });
+
+
+  // load tasks for the first time
+  loadTasks();
+
+$("#trash"). droppable({
+  accept: ".card .list-group-item",
+  tolernace: "touch",
+  drop: function(event, ui) {
+    ui.draggable.remove();
+    console.log("drop");
+  },
+  over: function(event, ui) {
+    console.log ("out");
   }
-  saveTasks();
-});
-
-// load tasks for the first time
-loadTasks();
+})
